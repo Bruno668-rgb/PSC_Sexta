f296bdb109a4dfb4eb7a4e43807cc9cb78cb7079
BrunoPacheco <limapacheco330@gmail.com>
2024-09-01 17:02:50 +0000

    Este programa calcula a quantidade de gorjeta que o garçom recebera, sendo calculada com base em 10%

---
+++ Gorjeta.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Gorjeta{
+    public static void main(String args[]) {
+        Scanner teclado = new Scanner(System.in);
+        double ValorConta, Gorjeta;
+
+        do {
+            System.out.println("Informe o valor da conta: ");
+            ValorConta = teclado.nextDouble();
+
+            if (ValorConta < 0) {
+                System.out.println("Conta inválida, ela não pode ser negativa.");
+            }
+        } while (ValorConta < 0); 
+
+        
+        Gorjeta = 0.10 * ValorConta;
+
+    System.out.println("O valor a ser pago de gorjeta é: " + Gorjeta );
+
+ teclado.close(); 
+    }
+}
